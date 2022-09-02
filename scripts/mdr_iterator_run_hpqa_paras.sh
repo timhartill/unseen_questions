1f67d64d1b624e2b54935c4c02f2b3f0eb46510d
timhartill <timhartill@gmail.com>
2022-09-02 14:56:22 +1200

    mdr: encode corpus hgx2 test10 hpqaabst

--- scripts/mdr_iterator_run_hpqa_paras.sh
+++ scripts/mdr_iterator_run_hpqa_paras.sh
@@ -84,13 +84,13 @@
 cd ../code
 
 python mdr_searchers.py \
-    --prefix ITER_hpqaabst_hovereval_test29_beam25_maxh4_hpqahovnqmubs24 \
+    --prefix ITER_hpqaabst_hovereval_test31_beam25_maxh4_hpqahovnqmubs200_mom \
     --output_dir $LDATA/out/mdr/logs \
     --predict_file $HDATA/data/baleen_downloads/hover/hover_qas_val_with_spfacts.jsonl \
-    --index_path $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test5-07-20-2022-nomom/index.npy \
-    --corpus_dict $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test5-07-20-2022-nomom/id2doc.json \
+    --index_path $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test9_mom_6gpubs200-08-28-2022-mom/index.npy \
+    --corpus_dict $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test9_mom_6gpubs200-08-28-2022-mom/id2doc.json \
     --model_name roberta-base \
-    --init_checkpoint $LDATA/out/mdr/logs/hover_hpqa_nq_mu_paras_test5-07-20-2022-nomom-seed16-bsz24-fp16True-lr2e-05-decay0.0-warm0.1-valbsz100-sharedTrue-ga1-varTrue-cenone/checkpoint_best.pt \
+    --init_checkpoint $LDATA/out/mdr/logs/hover_hpqa_nq_mu_paras_test9_mom_6gpubs200-08-28-2022-mom-seed16-bsz200-fp16True-lr1e-05-decay0.0-warm0.1-valbsz100-m0.999-k76800-t1.0-ga1-varTrue-cenone/checkpoint_q_last.pt \
     --model_name_stage google/electra-large-discriminator \
     --init_checkpoint_stage1 $LDATA/out/mdr/logs/stage1_test5_hpqa_hover_fever_new_sentMASKforcezerospweight1_fullevalmetrics-05-29-2022-rstage1-seed42-bsz12-fp16True-lr5e-05-decay0.0-warm0.1-valbsz100-ga8/checkpoint_best.pt \
     --init_checkpoint_stage2 $LDATA/out/mdr/logs/stage2_test3_hpqa_hover_fever_new_sentMASKforcezerospweight1_fevernegfix-06-14-2022-rstage2-seed42-bsz12-fp16True-lr5e-05-decay0.0-warm0.1-valbsz100-ga8/checkpoint_best.pt \
