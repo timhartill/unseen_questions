52d4110346a00ca207b1ac173f6029b6120e4e23
timhartill <timhartill@gmail.com>
2022-09-04 01:32:31 +1200

    ITER: update for full wiki hnsw build buffsize 40m

--- scripts/mdr_iterator_run_hpqa_paras.sh
+++ scripts/mdr_iterator_run_hpqa_paras.sh
@@ -75,6 +75,7 @@
 #     --stop_ev_thresh 0.91 \
 #    --stop_ansconfdelta_thresh 18.0
 #     --hnsw \
+#    --gpu_faiss \
 #     --stop_lowerev \
 # /large_data/thar011/out/mdr/encoded_corpora/hotpot/hotpot_qas_val_with_spfacts.jsonl \
 # /home/thar011/data/baleen_downloads/hover/hover_qas_val_with_spfacts.jsonl \
@@ -84,18 +85,18 @@
 cd ../code
 
 python mdr_searchers.py \
-    --prefix ITER_hpqaabst_hovereval_test34_beam25_maxh4_hpqahovnqmubs250_mom \
+    --prefix ITER_fullwiki_hovereval_test35_beam25_maxh4_hpqahovnqmubs250_mom \
     --output_dir $LDATA/out/mdr/logs \
     --predict_file $HDATA/data/baleen_downloads/hover/hover_qas_val_with_spfacts.jsonl \
-    --index_path $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_6gpubs250-09-02-2022/index.npy \
-    --corpus_dict $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_6gpubs250-09-02-2022/id2doc.json \
+    --index_path $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_fullwiki_6gpubs250-09-02-2022/index.npy \
+    --corpus_dict $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_fullwiki_6gpubs250-09-02-2022/id2doc.json \
     --model_name roberta-base \
     --init_checkpoint $LDATA/out/mdr/logs/hover_hpqa_nq_mu_paras_test12_mom_6gpubs250_hgx2-09-02-2022-mom-seed16-bsz250-fp16True-lr1e-05-decay0.0-warm0.1-valbsz100-m0.999-k76800-t1.0-ga1-varTrue-cenone/checkpoint_q_best.pt \
     --model_name_stage google/electra-large-discriminator \
     --init_checkpoint_stage1 $LDATA/out/mdr/logs/stage1_test5_hpqa_hover_fever_new_sentMASKforcezerospweight1_fullevalmetrics-05-29-2022-rstage1-seed42-bsz12-fp16True-lr5e-05-decay0.0-warm0.1-valbsz100-ga8/checkpoint_best.pt \
     --init_checkpoint_stage2 $LDATA/out/mdr/logs/stage2_test3_hpqa_hover_fever_new_sentMASKforcezerospweight1_fevernegfix-06-14-2022-rstage2-seed42-bsz12-fp16True-lr5e-05-decay0.0-warm0.1-valbsz100-ga8/checkpoint_best.pt \
     --gpu_model \
-    --gpu_faiss \
+    --hnsw \
     --hnsw_buffersize 40000000 \
     --save_index \
     --beam_size 25 \
