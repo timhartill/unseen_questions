2b42064f8b5a695ce11ee1dbe1b08b5fa0c73802
timhartill <timhartill@gmail.com>
2022-09-03 17:28:06 +1200

    mdr: iterator sh for ITER test 34

--- scripts/mdr_iterator_run_hpqa_paras.sh
+++ scripts/mdr_iterator_run_hpqa_paras.sh
@@ -84,13 +84,13 @@
 cd ../code
 
 python mdr_searchers.py \
-    --prefix ITER_hpqaabst_hovereval_test33_beam25_maxh4_hpqahovnqmubs150_mom \
+    --prefix ITER_hpqaabst_hovereval_test34_beam25_maxh4_hpqahovnqmubs250_mom \
     --output_dir $LDATA/out/mdr/logs \
     --predict_file $HDATA/data/baleen_downloads/hover/hover_qas_val_with_spfacts.jsonl \
-    --index_path $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test11_mom_6gpubs150_hgx2-09-01-2022/index.npy \
-    --corpus_dict $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test11_mom_6gpubs150_hgx2-09-01-2022/id2doc.json \
+    --index_path $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_6gpubs250-09-02-2022/index.npy \
+    --corpus_dict $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_6gpubs250-09-02-2022/id2doc.json \
     --model_name roberta-base \
-    --init_checkpoint $LDATA/out/mdr/logs/hover_hpqa_nq_mu_paras_test11_mom_6gpubs150_hgx2-09-01-2022-mom-seed16-bsz150-fp16True-lr1e-05-decay0.0-warm0.1-valbsz100-m0.999-k76800-t1.0-ga1-varTrue-cenone/checkpoint_q_last.pt \
+    --init_checkpoint $LDATA/out/mdr/logs/hover_hpqa_nq_mu_paras_test12_mom_6gpubs250_hgx2-09-02-2022-mom-seed16-bsz250-fp16True-lr1e-05-decay0.0-warm0.1-valbsz100-m0.999-k76800-t1.0-ga1-varTrue-cenone/checkpoint_q_best.pt \
     --model_name_stage google/electra-large-discriminator \
     --init_checkpoint_stage1 $LDATA/out/mdr/logs/stage1_test5_hpqa_hover_fever_new_sentMASKforcezerospweight1_fullevalmetrics-05-29-2022-rstage1-seed42-bsz12-fp16True-lr5e-05-decay0.0-warm0.1-valbsz100-ga8/checkpoint_best.pt \
     --init_checkpoint_stage2 $LDATA/out/mdr/logs/stage2_test3_hpqa_hover_fever_new_sentMASKforcezerospweight1_fevernegfix-06-14-2022-rstage2-seed42-bsz12-fp16True-lr5e-05-decay0.0-warm0.1-valbsz100-ga8/checkpoint_best.pt \
