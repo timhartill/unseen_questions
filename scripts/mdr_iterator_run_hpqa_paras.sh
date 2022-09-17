e3a073d920ed16fc6a525e834d046f0e811875ca
timhartill <timhartill@gmail.com>
2022-09-17 18:04:58 +1200

    ITER: run for csqa2 train with impl rels

--- scripts/mdr_iterator_run_hpqa_paras.sh
+++ scripts/mdr_iterator_run_hpqa_paras.sh
@@ -85,8 +85,8 @@
 # $UQA_DIR/aristotle_od_ans/test.tsv
 # $UQA_DIR/csqa2/dev.tsv
 # $UQA_DIR/creak_od_ans/dev.tsv
-# $UQA_DIR/creak_od_ans_impl_rels/dev.tsv
-# $UQA_DIR/csqa2_impl_rels/dev.tsv
+# $UQA_DIR/creak_od_ans_impl_rels/dev.tsv    $UQA_DIR/creak_od_ans_impl_rels/train.tsv
+# $UQA_DIR/csqa2_impl_rels/dev.tsv           $UQA_DIR/csqa2_impl_rels/train.tsv
 # $UQA_DIR/creak_initial_context/dev.tsv
 
 # HPQA Abstracts encoded against best retriever:
@@ -105,10 +105,10 @@
 cd ../code
 
 python mdr_searchers.py \
-    --prefix ITER_fullwiki_creaktrain_test48_b150_h4_hpqahovnqmubs250_mom \
+    --prefix ITER_fullwiki_csqa2train_test49_b150_h4_hpqahovnqmubs250_mom \
     --output_dir $LDATA/out/mdr/logs \
-    --output_dataset $UQA_DIR/creak_fullwiki_bs150_implrel/train.tsv \
-    --predict_file $UQA_DIR/creak_od_ans_impl_rels/train.tsv \
+    --output_dataset $UQA_DIR/csqa2_fullwiki_bs150_implrel/train.tsv \
+    --predict_file $UQA_DIR/csqa2_impl_rels/train.tsv \
     --index_path $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_fullwiki_6gpubs250-09-02-2022/index.npy \
     --corpus_dict $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_fullwiki_6gpubs250-09-02-2022/id2doc.json \
     --model_name roberta-base \
