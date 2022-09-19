c44d05bf4d81eda7aac41e0321cdac724a8acf1f
timhartill <timhartill@gmail.com>
2022-09-19 23:46:47 +1200

    ITER: run retrieval for musique_qa train v2 using musique_full

--- scripts/mdr_iterator_run_hpqa_paras.sh
+++ scripts/mdr_iterator_run_hpqa_paras.sh
@@ -108,10 +108,10 @@
 cd ../code
 
 python mdr_searchers.py \
-    --prefix ITER_fullwiki_mudev_test55_b60_h4_hpqahovnqmubs250_mom \
+    --prefix ITER_fullwiki_mutrain_test54_b60_h4_hpqahovnqmubs250_mom \
     --output_dir $LDATA/out/mdr/logs \
-    --output_dataset $UQA_DIR/musique_qa_fullwiki_bs60/dev.tsv \
-    --predict_file $UQA_DIR/musique_full_qa_od_ans/dev.tsv \
+    --output_dataset $UQA_DIR/musique_qa_fullwiki_bs60/train.tsv \
+    --predict_file $UQA_DIR/musique_full_qa_od_ans/train.tsv \
     --index_path $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_fullwiki_6gpubs250-09-02-2022/index.npy \
     --corpus_dict $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_fullwiki_6gpubs250-09-02-2022/id2doc.json \
     --model_name roberta-base \
