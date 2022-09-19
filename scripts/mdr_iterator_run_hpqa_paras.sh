24978966f6c082ef4ec9ea0d30ea35e90b55a708
timhartill <timhartill@gmail.com>
2022-09-20 10:59:57 +1200

    ITER: run retrieval for qasc train

--- scripts/mdr_iterator_run_hpqa_paras.sh
+++ scripts/mdr_iterator_run_hpqa_paras.sh
@@ -91,6 +91,7 @@
 # $UQA_DIR/nq_open_od_ans/dev.tsv            $UQA_DIR/nq_open_od_ans/train.tsv
 # $UQA_DIR/hpqa_od_ans/dev.tsv            $UQA_DIR/hpqa_od_ans/train.tsv
 # $UQA_DIR/musique_full_qa_od_ans/dev.tsv            $UQA_DIR/musique_full_qa_od_ans/train.tsv
+# $UQA_DIR/qasc/dev.tsv            $UQA_DIR/qasc/train.tsv
 
 # HPQA Abstracts encoded against best retriever:
 # $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_6gpubs250-09-02-2022/index.npy
@@ -108,10 +109,10 @@
 cd ../code
 
 python mdr_searchers.py \
-    --prefix ITER_fullwiki_mutrain_test54_b60_h4_hpqahovnqmubs250_mom \
+    --prefix ITER_fullwiki_qasctrain_test56_b60_h4_hpqahovnqmubs250_mom \
     --output_dir $LDATA/out/mdr/logs \
-    --output_dataset $UQA_DIR/musique_qa_fullwiki_bs60/train.tsv \
-    --predict_file $UQA_DIR/musique_full_qa_od_ans/train.tsv \
+    --output_dataset $UQA_DIR/qasc_fullwiki_bs60/train.tsv \
+    --predict_file $UQA_DIR/qasc/train.tsv \
     --index_path $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_fullwiki_6gpubs250-09-02-2022/index.npy \
     --corpus_dict $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_fullwiki_6gpubs250-09-02-2022/id2doc.json \
     --model_name roberta-base \
