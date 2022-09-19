26479c406cef536c67fc519631c3aebf798998ab
timhartill <timhartill@gmail.com>
2022-09-19 23:37:27 +1200

    ITER: run retrieval for musique_qa dev v2 using musique_full

--- scripts/mdr_iterator_run_hpqa_paras.sh
+++ scripts/mdr_iterator_run_hpqa_paras.sh
@@ -90,7 +90,7 @@
 # $UQA_DIR/creak_initial_context/dev.tsv
 # $UQA_DIR/nq_open_od_ans/dev.tsv            $UQA_DIR/nq_open_od_ans/train.tsv
 # $UQA_DIR/hpqa_od_ans/dev.tsv            $UQA_DIR/hpqa_od_ans/train.tsv
-# $UQA_DIR/musique_qa/dev.tsv            $UQA_DIR/musique_qa/train.tsv
+# $UQA_DIR/musique_full_qa_od_ans/dev.tsv            $UQA_DIR/musique_full_qa_od_ans/train.tsv
 
 # HPQA Abstracts encoded against best retriever:
 # $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_6gpubs250-09-02-2022/index.npy
@@ -111,7 +111,7 @@ python mdr_searchers.py \
     --prefix ITER_fullwiki_mudev_test55_b60_h4_hpqahovnqmubs250_mom \
     --output_dir $LDATA/out/mdr/logs \
     --output_dataset $UQA_DIR/musique_qa_fullwiki_bs60/dev.tsv \
-    --predict_file $UQA_DIR/musique_qa/dev.tsv \
+    --predict_file $UQA_DIR/musique_full_qa_od_ans/dev.tsv \
     --index_path $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_fullwiki_6gpubs250-09-02-2022/index.npy \
     --corpus_dict $LDATA/out/mdr/encoded_corpora/hover_hpqa_nq_mu_paras_test12_mom_fullwiki_6gpubs250-09-02-2022/id2doc.json \
     --model_name roberta-base \
