49babf67a3051aa6061a3794cb1c261a640a82d0
timhartill <timhartill@gmail.com>
2022-10-03 17:29:16 +1300

    ITER: add mu dev initial context retrieval to MDR retrieval version

--- scripts/mdr_iterator_run_hpqa_paras_ALL_EVAL_MDR.sh
+++ scripts/mdr_iterator_run_hpqa_paras_ALL_EVAL_MDR.sh
@@ -336,4 +336,44 @@ python mdr_searchers.py \
     --stop_ansconfdelta_thresh 99999.0
 
 
+echo "BUILD CONTEXT FOR MUSQUE MU DEV INITIAL CONTEXT..."
+python mdr_searchers.py \
+    --prefix ITER_fullwiki_mudev_test81ic_b150_h4_mdr_orig \
+    --output_dir $LDATA/out/mdr/logs \
+    --output_dataset $UQA_DIR/musique_mu_dev_inital_contextv2_fullwiki_bs150_mdr/dev.tsv \
+    --predict_file $UQA_DIR/musique_mu_dev_inital_contextv2/dev.tsv \
+    --index_path $LDATA/out/mdr/encoded_corpora/full_wiki_mdr_orig_ckpt_8gpu_bs150/index.npy \
+    --corpus_dict $LDATA/out/mdr/encoded_corpora/full_wiki_mdr_orig_ckpt_8gpu_bs150/id2doc.json \
+    --model_name roberta-base \
+    --init_checkpoint $LDATA/out/mdr/logs/hpqa_mdr_orig_ckpt_8gpu_bs150/q_encoder.pt \
+    --model_name_stage google/electra-large-discriminator \
+    --init_checkpoint_stage1 $LDATA/out/mdr/logs/stage1_test5_hpqa_hover_fever_new_sentMASKforcezerospweight1_fullevalmetrics-05-29-2022-rstage1-seed42-bsz12-fp16True-lr5e-05-decay0.0-warm0.1-valbsz100-ga8/checkpoint_best.pt \
+    --init_checkpoint_stage2 $LDATA/out/mdr/logs/stage2_test3_hpqa_hover_fever_new_sentMASKforcezerospweight1_fevernegfix-06-14-2022-rstage2-seed42-bsz12-fp16True-lr5e-05-decay0.0-warm0.1-valbsz100-ga8/checkpoint_best.pt \
+    --gpu_model \
+    --hnsw \
+    --hnsw_buffersize 40000000 \
+    --save_index \
+    --beam_size 150 \
+    --predict_batch_size 160 \
+    --query_add_titles \
+    --topk 9 \
+    --topk_stage2 5 \
+    --s1_use_para_score \
+    --s1_para_sent_ratio 0.5 \
+    --s1_para_sent_ratio_final -1.0 \
+    --s2_use_para_score \
+    --s2_para_sent_ratio 0.5 \
+    --s2_para_sent_ratio_final -1.0 \
+    --max_hops 4 \
+    --max_q_len 70 \
+    --max_q_sp_len 512 \
+    --max_c_len 512 \
+    --max_ans_len 35 \
+    --s2_sp_thresh 0.10 \
+    --s2_min_take 2 \
+    --stop_ev_thresh 1.01 \
+    --stop_ansconfdelta_thresh 99999.0
+
+
+
 
