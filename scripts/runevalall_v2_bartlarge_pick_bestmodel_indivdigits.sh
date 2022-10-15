125bdd835ac6bbed6bb11df5d0f134386060ddf6
timhartill <timhartill@gmail.com>
2022-10-15 21:29:48 +1300

    UQA: add UQA_s11_v6_all_g1_qa_g2_numlit_wikissvise_from_s9_v2_addretdsv2

--- scripts/runevalall_v2_bartlarge_pick_bestmodel_indivdigits.sh
+++ scripts/runevalall_v2_bartlarge_pick_bestmodel_indivdigits.sh
@@ -3,6 +3,8 @@
 # use for bart-large on base uqa datasets when not selecting a particular checkpoint
 # run predictions on multiple dev and test sets using test set where available, dev set otherwise
 
+#         --fp16 \  Almost the same as tf32 on inference
+
 cd ../code
 
 echo "Running Eval using ind digit tokenization for best model in $1 ..."
@@ -17,7 +19,6 @@ python cli.py --output_dir $1 \
         --dont_pretokenize \
         --dont_save_train_token_file \
         --max_output_length 130 \
-        --fp16 \
         --do_predict_all --calc_metrics_all --add_only_missing
 
 
