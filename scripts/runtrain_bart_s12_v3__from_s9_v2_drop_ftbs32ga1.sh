e6c877ef021b49aa7b3cdac23d5ba3d5655846c0
timhartill <timhartill@gmail.com>
2022-10-17 12:41:00 +1300

    UQA: add bart drop ft script bs 32 ga 1

---
+++ scripts/runtrain_bart_s12_v3__from_s9_v2_drop_ftbs32ga1.sh
@@ -0,0 +1,40 @@
+# training command
+
+# CHANGE g2_prob from 0.2->0.3 and remove mu dev fullwiki from training mix
+
+#        --train_batch_size 32 \
+#        --fp16 \
+#         --dont_pretokenize \
+#        --error_based_sampling \
+#        --indiv_digits \
+#         --num_workers 10 \
+#         --num_workers 0 \    default is 0 so no need to include
+#         --add_mask_char NONE \  made this the default so excluding here
+
+cd ../code
+
+python cli.py --do_train --output_dir $LDATA/out/mdr/logs/UQA_s12_v3__from_s9_v2_drop_ft_bart_bs32ga1 \
+        --is_unifiedqa \
+        --train_file $UDATA/data/unifiedqa/train.tsv \
+        --predict_file $UDATA/data/unifiedqa/dev.tsv \
+        --checkpoint $LDATA/out/mdr/logs/UQA_s9_v2_numlit_wikissvise_idt_errsamp_fixdecode/best-model.pt \
+        --train_batch_size 32 \
+        --predict_batch_size 32 \
+        --append_another_bos --do_lowercase \
+        --eval_period 10000 --verbose \
+        --gradient_accumulation_steps 1 \
+        --wait_step 10 \
+        --num_scheduler_steps 150000 \
+        --learning_rate 2e-5 \
+        --model facebook/bart-large \
+        --seed 42 \
+        --ssm_prob 0.65 \
+        --add_mask_char NONE \
+        --max_output_length 130 \
+        --fp16 \
+        --dont_pretokenize \
+        --dont_save_train_token_file \
+        --indiv_digits \
+        --mixture drop
+        
+
