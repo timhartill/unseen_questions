2ba124713b1d3dc53a74cd9fe2d8989c60d6680d
timhartill <timhartill@gmail.com>
2022-10-19 13:09:00 +1300

    UQA: add train scripts for bart ft grop iircg from s11_v3cont1m

---
+++ scripts/runtrain_bart_s12_v6__from_s11_v3cont1m_drop_ft.sh
@@ -0,0 +1,41 @@
+# training command
+
+# CHANGE g2_prob from 0.2->0.3 and remove mu dev fullwiki from training mix
+
+#        --train_batch_size 32 \
+#        --fp16 \
+#         --dont_pretokenize \
+#        --error_based_sampling \
+#        --indiv_digits \
+#         --num_workers 10 \
+#         --num_workers 0 \    default is 0 so no need to include
+#         --add_mask_char NONE \  made this the default so excluding here
+
+
+cd ../code
+
+python cli.py --do_train --output_dir $LDATA/out/mdr/logs/UQA_s12_v6__from_s11_v3cont1m_drop_ft_bart \
+        --is_unifiedqa \
+        --train_file $UDATA/data/unifiedqa/train.tsv \
+        --predict_file $UDATA/data/unifiedqa/dev.tsv \
+        --checkpoint $LDATA/out/mdr/logs/UQA_s11_v3_all_g1_qa_g2_numlit_wikissvise_from_s9_v2_addretds_CONTINUE1m/best-model.pt \
+        --train_batch_size 32 \
+        --predict_batch_size 32 \
+        --append_another_bos --do_lowercase \
+        --eval_period 10000 --verbose \
+        --gradient_accumulation_steps 4 \
+        --wait_step 10 \
+        --num_scheduler_steps 150000 \
+        --learning_rate 2e-5 \
+        --model facebook/bart-large \
+        --seed 42 \
+        --ssm_prob 0.65 \
+        --add_mask_char NONE \
+        --max_output_length 130 \
+        --fp16 \
+        --dont_pretokenize \
+        --dont_save_train_token_file \
+        --indiv_digits \
+        --mixture drop
+        
+
