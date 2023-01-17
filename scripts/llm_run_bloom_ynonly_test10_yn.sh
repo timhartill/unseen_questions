424a3627064a681d829bd45ba88f9c8028843b71
timhartill <timhartill@gmail.com>
2023-01-17 23:03:12 +1300

    LLM: test longer yn prompts v2

---
+++ scripts/llm_run_bloom_ynonly_test10_yn.sh
@@ -0,0 +1,23 @@
+# Abduce rationale from LLM
+# run on 3 80GB gpus, will take ~75 GB on 2 and ~40GB on the third
+
+#     --generate_train \
+
+cd ../code
+
+python llm_infer_output.py \
+    --prefix LLM_TEST10_YNONLY_addcsqa2_hpqa_instn \
+    --output_dir $LDATA/out/mdr/logs \
+    --model_name bigscience/bloom \
+    --max_new_tokens 128 \
+    --max_seq_len_in 1152 \
+    --generate_dev \
+    --generate_eval \
+    --debug \
+    --max_samples 30 \
+    --num_beams 1 \
+    --num_return_sequences 1 \
+    --max_memory_buffer 6 \
+    --max_memory -1
+
+
