c95ed59abda83b730c6199076cd1fd34be59e3c8
timhartill <timhartill@gmail.com>
2023-01-30 15:11:45 +1300

    LLM: creak combine llm + iterator

--- scripts/llm_combine_contexts_creak.sh
+++ scripts/llm_combine_contexts_creak.sh
@@ -3,7 +3,7 @@
 cd ../code
 
 python combine_llm_retrieved_contexts.py \
-        --llm_dataset creak_bigbench_llm_expl \
+        --llm_dataset creak_llm_expl \
         --iter_dataset creak_fullwiki_bs150_noimplrel \
         --output_dataset creak_llm_expl_fullwiki_bs150_noimplrel
         
