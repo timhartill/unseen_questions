3ec55be80e27348f145c1f7425c962199df6e21b
timhartill <timhartill@gmail.com>
2023-04-29 16:09:00 +1200

    UQA: train s11 v11 1 stage, no numerical literacy datasets

---
+++ scripts/runtrain_bart_s11_v11_no_numlit_1stage_1group.sh
@@ -0,0 +1,40 @@
+# training command
+
+#        --train_batch_size 32 \
+#        --fp16 \
+#         --dont_pretokenize \
+#        --error_based_sampling \
+#        --indiv_digits \
+#         --num_workers 10 \
+#         --num_workers 0 \    default is 0 so no need to include
+#         --add_mask_char NONE \  made this the default so excluding here
+
+cd ../code
+
+python cli.py --do_train --output_dir $LDATA/out/mdr/logs/UQA_11_v11_NOnumlit_withwikissvise_idt_errsamp_1group_1stage \
+        --is_unifiedqa \
+        --train_file $UDATA/data/unifiedqa/train.tsv \
+        --predict_file $UDATA/data/unifiedqa/dev.tsv \
+        --train_batch_size 32 \
+        --predict_batch_size 32 \
+        --append_another_bos --do_lowercase \
+        --eval_period 10000 --verbose \
+        --gradient_accumulation_steps 4 \
+        --wait_step 100 \
+        --num_scheduler_steps 250000 \
+        --learning_rate 2e-5 \
+        --model facebook/bart-large \
+        --seed 42 \
+        --ssm_prob 0.65 \
+        --add_mask_char NONE \
+        --max_output_length 130 \
+        --fp16 \
+        --dont_pretokenize \
+        --dont_save_train_token_file \
+        --indiv_digits \
+        --error_based_sampling \
+        --error_based_ssvise_prob 0.05 \
+        --approx_dev_samples 1250 \
+        --mixture enwiki_20200801_selfsvised,q_paras_all,q_paras_noanswer_all,q_mc_all,q_mc_paras_all,q_od_all,enwiki_20200801_selfsvised,q_ret_paras_all,q_ret_paras_maxp4_all
+
+
